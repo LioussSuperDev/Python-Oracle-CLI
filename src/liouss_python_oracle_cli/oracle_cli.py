2708b545c8b0ae8add05fe5ca54ed2db6a20eaf5
jdiscala <jules.di-scala@univ-tlse3.fr>
2026-02-17 15:13:08 +0100

    little fix

--- src/liouss_python_oracle_cli/oracle_cli.py
+++ src/liouss_python_oracle_cli/oracle_cli.py
@@ -1,7 +1,7 @@
 import json
 import os
 from typing import Optional
-from liouss_python_toolkit.printer import beautiful_print
+from liouss_python_toolkit.printer import beautiful_print, GREEN_COLOR, ORANGE_COLOR, RED_COLOR, LIGHT_BLUE_COLOR
 from liouss_python_toolkit.utility import edit_in_editor
 from liouss_python_toolkit.utility import real_path
 from liouss_python_sql_connectors.sql_connection import SQLConnection
@@ -48,7 +48,7 @@ class OracleCmd(cmd.Cmd):
         for i, query in enumerate(queries):
             self.query_oracle(identifiers, query.strip(";\n\r "), False, task_id=task_id, sub_task_id=i, default_connection=self.connection)
     
-    def query_oracle(self, identifiers, query, commit, task_id=None, sub_task_id=0, default_connection=None, placeholders=None):
+    def query_oracle(self, identifiers, query, commit, task_id=None, sync=False, sub_task_id=0, default_connection=None, placeholders=None):
         
         if task_id is None:
             task_id = "NOT_A_TASK"
@@ -77,22 +77,31 @@ class OracleCmd(cmd.Cmd):
                         result = connection.query_one(query.strip("\n\r"), print_error=False, ignore_errors=False, include_col_name=True)
                     else:
                         result = connection.query_one(query.strip("\n\r"), placeholders, print_error=False, ignore_errors=False, include_col_name=True)
-                    beautiful_print(f"Query complete.", log_only=True, log=log_file)
+                    beautiful_print(f"Query complete.", log_only=(not sync), log=log_file)
                 except Exception as e:
-                    beautiful_print(f"Error executing query: {query}", log_only=True, log=log_file)
+                    beautiful_print(f"Error executing query: {query}", log_only=(not sync), log=log_file, color=RED_COLOR)
                     stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-                    beautiful_print(stack, log_only=True, log=log_file)
+                    beautiful_print(stack, log_only=(not sync), log=log_file, color=RED_COLOR)
                     return
                 
                 if commit:
                     connection.get_db().commit()
+                    
                 with open(output_file, "w+", newline="") as f:
                     csv.writer(f).writerows(result or [])
+                if sync:
+                    with open(output_file, "r",) as f:
+                        readlines = f.readlines(1000)
+                        if len(readlines) > 0:
+                            beautiful_print("")
+                            for line in readlines:
+                                beautiful_print(line.strip(" \n\r"), color=LIGHT_BLUE_COLOR)
+                            beautiful_print("")
                 
         except Exception as e:
-            beautiful_print(f"Error executing query: {task_id}", log_only=True, log=log_file)
+            beautiful_print(f"Error executing query: {task_id}", log_only=(not sync), log=log_file, color=RED_COLOR)
             stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-            beautiful_print(stack, log_only=True, log=log_file)
+            beautiful_print(stack, log_only=(not sync), log=log_file, color=RED_COLOR)
 
     def insert_many(self, identifiers, file_path, table_name, buffer_size, task_id=None, sub_task_id=0, default_connection=None):
         if task_id is None:
@@ -144,22 +153,30 @@ class OracleCmd(cmd.Cmd):
                         beautiful_print(f"Commit.", log=log_file, log_only=True)
                             
                 except Exception as e:
-                    beautiful_print(f"Inserting file: {file_path}", log_only=True, log=log_file)
+                    beautiful_print(f"Inserting file: {file_path}", log_only=True, log=log_file, color=RED_COLOR)
                     stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-                    beautiful_print(stack, log_only=True, log=log_file)
+                    beautiful_print(stack, log_only=True, log=log_file, color=RED_COLOR)
                     return
                 
                 
                 connection.get_db().commit()
         except Exception as e:
-            beautiful_print(f"Error executing query: {task_id}", log_only=True, log=log_file)
+            beautiful_print(f"Error executing query: {task_id}", log_only=True, log=log_file, color=RED_COLOR)
             stack = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-            beautiful_print(stack, log_only=True, log=log_file)
+            beautiful_print(stack, log_only=True, log=log_file, color=RED_COLOR)
 
-    def start_task(self, description, func, *args, **kwargs):
-        task_id = f"async_{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}"
-        self.tasks[task_id] = {"description": description, "process":self.pool.submit(func, *args, task_id=task_id, **kwargs), "connection":None}
-        beautiful_print(f"Started task {task_id}: {description}")
+    def start_task(self, description, func, sync, *args, **kwargs):
+        
+        if not sync:
+            task_id = f"async_{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}"
+            self.tasks[task_id] = {"description": description, "process":self.pool.submit(func, *args, task_id=task_id, sync=sync, **kwargs), "connection":None}
+            beautiful_print(f"Started task {task_id}: {description}")
+        else:
+            task_id = f"sync_{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}"
+            beautiful_print(f"Starting task {task_id}: {description}")
+            self.tasks[task_id] = {"description": description, "process":None, "connection":self.connection}
+            func(*args, task_id=task_id, sync=sync, **kwargs)
+        
         return task_id
     
     def do_query(self, arg):
@@ -167,42 +184,48 @@ class OracleCmd(cmd.Cmd):
         Usage: query <SQL_QUERY>"""
         if arg.strip() == "":
             arg = edit_in_editor("Type your query on the line below", ignore_lines=1)
-        self.start_task(f"query {arg}", self.query_oracle, self.oracle_identifiers, arg, False)
+        self.start_task(f"query {arg}", self.query_oracle, False, self.oracle_identifiers, arg, False)
+    
+    def do_querys(self, arg):
+        """Execute a SQL query on the Oracle database synchronously.
+        Usage: querys <SQL_QUERY>"""
+        if arg.strip() == "":
+            arg = edit_in_editor("Type your query on the line below", ignore_lines=1)
+        self.start_task(f"query {arg}", self.query_oracle, True, self.oracle_identifiers, arg, False)
     
     def do_queryc(self, arg):
         """Execute a SQL query on the Oracle database. Commits after execution
         Usage: queryc <SQL_QUERY>"""
         if arg.strip() == "":
             arg = edit_in_editor("Type your query on the line below", ignore_lines=1)
-        self.start_task(f"queryc {arg}", self.query_oracle, self.oracle_identifiers, arg, True)
+        self.start_task(f"queryc {arg}", self.query_oracle, False, self.oracle_identifiers, arg, True)
         
     def do_taskls(self, arg):
         """List all running tasks.
-        Usage: tasklst [-i]"""
+        Usage: taskls [-i]"""
         for task_id, task_info in self.tasks.items():
-            status = "Running" if not task_info["process"].done() else "Done"
+            status = "Running" if task_info["process"] and (not task_info["process"].done()) else "Done"
+            color = GREEN_COLOR if status == "Done" else ORANGE_COLOR
             if arg != "-i" or status == "Running":
                 sid,serial = self.tasks[task_id]["SID"],self.tasks[task_id]["SERIAL"]
-                beautiful_print(f"[{task_id}][{sid},{serial}]: ({status}) {task_info['description']}")
+                beautiful_print(f"[{task_id}][{sid},{serial}]: ({status}) {task_info['description']}", color=color)
     
     def do_stoptsk(self, arg):
         """Stop a running task.
         Usage: stoptsk <TASK_ID>"""
-        if arg in self.tasks:
-            self.tasks[arg]["process"].cancel()
-            beautiful_print(f"[Oracle] Stopped task {arg}: {self.tasks[arg]['description']}")
-        else:
-            beautiful_print(f"No task found with ID: {arg}")
-    
-    
-    def do_querysync(self, arg):
-        """Execute a SQL query on the Oracle database synchronously.
-        Usage: querysync <SQL_QUERY>"""
-        if arg.strip() == "":
-            arg = edit_in_editor("Type your query on the line below", ignore_lines=1)
-        task_id = f"sync_{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}"
-        beautiful_print(f"Computing query {task_id}: {arg}")
-        self.query_oracle(self.oracle_identifiers, arg, False, task_id=task_id, default_connection=self.connection)
+        
+        if not arg in self.tasks:
+            beautiful_print(f"No task found with ID: {arg}", color=RED_COLOR)
+            return
+        
+        if not self.tasks[arg]["process"]:
+            beautiful_print(f"Cannot stop sync task", color=RED_COLOR)
+            return
+
+        self.tasks[arg]["process"].cancel()
+        beautiful_print(f"Stopped task {arg}: {self.tasks[arg]['description']}")
+
+            
     
     def do_insertmany(self, arg):
         """Inserts data in database from a file.
@@ -211,8 +234,8 @@ class OracleCmd(cmd.Cmd):
         arg2 = arg
         arg = shlex.split(arg)
         if len(arg) != 2:
-            beautiful_print(f"Expected args: 2. Received: {len(arg)}.")
-        self.start_task(f"insertmany {arg2}", self.insert_many, self.oracle_identifiers, real_path(arg[0]), arg[1], 50000)
+            beautiful_print(f"Expected args: 2. Received: {len(arg)}.", color=RED_COLOR)
+        self.start_task(f"insertmany {arg2}", self.insert_many, False, self.oracle_identifiers, real_path(arg[0]), arg[1], 50000)
         
     def do_runscript(self, arg):
         """Run a SQL script from a file.
@@ -221,19 +244,19 @@ class OracleCmd(cmd.Cmd):
         if arg and arg.strip() != "":
             arg = real_path(arg)
             if not os.path.isfile(arg):
-                beautiful_print(f"File not found: {arg}")
+                beautiful_print(f"File not found: {arg}", color=RED_COLOR)
                 return
             with open(arg, "r") as f:
                 script = f.read()
         else:
             script = edit_in_editor("Type your script on the line below", ignore_lines=1)
             
-        self.start_task(f"runscript {arg}", self.runscript_oracle, self.oracle_identifiers, script)
+        self.start_task(f"runscript {arg}", self.runscript_oracle, False, self.oracle_identifiers, script)
         
     def do_exit(self, arg):
         """Exit the Oracle prompt."""
         for task_id in list(self.tasks.keys()):
-            if not self.tasks[task_id]["process"].done():
+            if self.tasks[task_id]["process"] and (not self.tasks[task_id]["process"].done()):
                 self.do_stoptsk(task_id)
         return True
     
@@ -242,7 +265,7 @@ class OracleCmd(cmd.Cmd):
         Usage: savecmd <CMD_NAME>"""
         
         if arg.strip() == "":
-            beautiful_print("Please specify the command name")
+            beautiful_print("Please specify the command name", color=RED_COLOR)
         
         os.makedirs(os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -270,12 +293,12 @@ class OracleCmd(cmd.Cmd):
         for s in os.listdir(path_to_list_command):
             beautiful_print(s[:-4])
             
-    def do_runcmd(self, arg):
+    def do_aruncmd(self, arg):
         """Runs a user command
         USAGE: runcmd <CMD_NAME> [args...]"""
         args = shlex.split(arg)
         if not args:
-            beautiful_print("Please specify the command name")
+            beautiful_print("Please specify the command name", color=RED_COLOR)
         
         os.makedirs(os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -286,13 +309,40 @@ class OracleCmd(cmd.Cmd):
             "commands",
             f"{args[0]}.sql"
         )
+        if not os.path.exists(path_to_load_command):
+            beautiful_print("Unknown command. listcmd to get the list of available commands", color=RED_COLOR)
+            return
         with open(path_to_load_command,"r") as f:
             command = f.read()
-            self.start_task(f"cmd {arg}", self.query_oracle, self.oracle_identifiers, command, False, placeholders=tuple(args[1:]) if len(args) > 1 else None)
-            
+            self.start_task(f"aruncmd {arg}", self.query_oracle, False, self.oracle_identifiers, command, False, placeholders=tuple(args[1:]) if len(args) > 1 else None)
+    
+    def do_runcmd(self, arg):
+        """Runs a user command
+        USAGE: runcmd <CMD_NAME> [args...]"""
+        args = shlex.split(arg)
+        if not args:
+            beautiful_print("Please specify the command name", color=RED_COLOR)
+        
+        os.makedirs(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "commands"
+        ), exist_ok=True)   
+        path_to_load_command = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "commands",
+            f"{args[0]}.sql"
+        )
+        if not os.path.exists(path_to_load_command):
+            beautiful_print("Unknown command. listcmd to get the list of available commands", color=RED_COLOR)
+            return
+        with open(path_to_load_command,"r") as f:
+            command = f.read()
+            self.start_task(f"runcmd {arg}", self.query_oracle, True, self.oracle_identifiers, command, False, placeholders=tuple(args[1:]) if len(args) > 1 else None)
+        
+        
 def main():
     beautiful_print("~~~----~~~")
-    beautiful_print("Oracle CLI V0.1.0")
+    beautiful_print("Oracle CLI V0.1.1")
     beautiful_print("Author: Liouss")
     beautiful_print("~~~----~~~")
     
